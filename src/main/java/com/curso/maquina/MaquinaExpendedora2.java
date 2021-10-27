0bbd88d4d13f4e33a75f7c888509c4d090deaf75
begonaOlea <bolea@programia.es>
2021-10-27 11:51:34 +0200

    maquina refrescos 2

--- src/main/java/com/curso/maquina/MaquinaExpendedora2.java
+++ src/main/java/com/curso/maquina/MaquinaExpendedora2.java
@@ -7,15 +7,16 @@ public class MaquinaExpendedora2 {
 	private int cantidadVendida;
 	
 	private double cambios;
-	private Refresco refresco;
+	private Refresco[] refrescos; //3 
 	
-	public MaquinaExpendedora2(Refresco refresco, 
+	public MaquinaExpendedora2(Refresco[] refrescos, 
 			double cambiosIni) {
 		this.recaudacion = 0.0;
 		this.recaudacion = 0;
 		this.cambios = cambiosIni;
-		this.refresco = refresco;
+		this.refrescos = refrescos ;
 	}
+	
 	/**
 	 * Metodo que vende 1 refresco. Recibe el dinero
 	 * que introduce el usuario en la maquina
@@ -26,15 +27,16 @@ public class MaquinaExpendedora2 {
 	 * @param dineroIng
 	 * @return
 	 */
-	public double vender(double dineroIng){
+	public double vender(double dineroIng, int posicion){
 		//validar datos entrada
 		//PENDIENTE
+		
 		//validar stock
-		if(this.refresco.estaAgotado()) {
+		if(this.refrescos[posicion].estaAgotado()) {
 			return -1;
 		}
 		//validar si hay cambio suficiente
-		double cambioNec = dineroIng - this.refresco.getPrecioUnidad();
+		double cambioNec = dineroIng - this.refrescos[posicion].getPrecioUnidad();
 		if(cambioNec < 0 ) {
 			return -3; //no dinero suficiente
 		}
@@ -44,16 +46,16 @@ public class MaquinaExpendedora2 {
 		
 		//todo ok
 		this.cantidadVendida ++;
-		this.recaudacion += this.refresco.getPrecioUnidad();
-		this.refresco.entregar(); //resta 1 al stock ref
-		this.cambios += this.refresco.getPrecioUnidad(); 
+		this.recaudacion += this.refrescos[posicion].getPrecioUnidad();
+		this.refrescos[posicion].entregar(); //resta 1 al stock ref
+		this.cambios += this.refrescos[posicion].getPrecioUnidad(); 
 		
 		return cambioNec;
 	}
 	
 	
-	public void setRefresco(Refresco refresco) {
-		this.refresco = refresco;
+	public void reponerRefresco(Refresco refresco, int posicion) {
+		this.refrescos[posicion] = refresco;
 	}
 	
 	public double getCambios() {
